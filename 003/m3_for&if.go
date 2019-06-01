d343a4740da5c2b095ba1e04db2e6756182b57e1
zaur <1zaur1@mail.ru>
2019-06-01 14:49:43 +0300

    003-002 - m2 m3

---
+++ 003/m3_for&if.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	for i :=1; i <= 10; i++ {
+		if i%2 == 0{
+			fmt.Println(i, "- четное")
+		}else{
+			fmt.Println(i, "- нечетное")
+		}
+		
+		switch i {
+			case 0: fmt.Println("Zero")
+			case 1: fmt.Println("One")
+			case 2: fmt.Println("Two")
+			case 3: fmt.Println("Three")
+			case 4: fmt.Println("Four")
+			case 5: fmt.Println("Five")
+			default: fmt.Println("Unknown Number")
+			}
+	} 
+}
